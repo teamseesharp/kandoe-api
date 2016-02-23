899538c641d7545227c69b1bd1c93cdef321e648
cas decelle <cas.decelle@gmail.com>
2016-02-23 12:19:22 +0100

    test database connection

--- Data/EFDB/Connection/Initializer.cs
+++ Data/EFDB/Connection/Initializer.cs
@@ -2,7 +2,7 @@
 using System.Data.Entity;
 
 namespace Kandoe.Data.EFDB.Connection {
-    public class Initializer : CreateDatabaseIfNotExists<Context> {
+    public class Initializer : DropCreateDatabaseAlways<Context> {
         protected override void Seed(Context context) {
             //base.Seed(context);
             var card = new Card("testImage", 0, "testText");
