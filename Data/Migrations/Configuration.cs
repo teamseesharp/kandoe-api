1a3f4d718692e0359df656c2e3f4f399f8cc95d2
Michelle Beckers <michellebeckers@hotmail.com>
2016-03-12 22:53:59 +0100

    STOMME API SESSIE CALL

--- Data/Migrations/Configuration.cs
+++ Data/Migrations/Configuration.cs
@@ -163,7 +163,7 @@ namespace Kandoe.Data.Migrations {
             session.Id = ++sessionId;
             context.Sessions.AddOrUpdate(session);
 
-            session = new Session(false, 3, false, 3, 8, Modus.Async, 3, 0, 7, DateTime.Now, DateTime.Today);
+            session = new Session(false, 3, true, 3, 8, Modus.Async, 3, 0, 7, DateTime.Now, DateTime.Today);
             session.Id = ++sessionId;
             context.Sessions.AddOrUpdate(session);
 
