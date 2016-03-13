c03e3a1b07be01d84831ea31a6729c6dc8911df8
Michelle Beckers <michellebeckers@hotmail.com>
2016-03-13 14:06:12 +0100

    seed

--- Data/Migrations/Configuration.cs
+++ Data/Migrations/Configuration.cs
@@ -167,7 +167,7 @@ namespace Kandoe.Data.Migrations {
             session.Id = ++sessionId;
             context.Sessions.AddOrUpdate(session);
 
-            session = new Session(false, 1, false, 3, 8, Modus.Async, 3, 0, 7, DateTime.Now, DateTime.Today);
+            session = new Session(true, 1, false, 3, 8, Modus.Async, 3, 0, 7, DateTime.Now, DateTime.Today);
             session.Id = ++sessionId;
             context.Sessions.AddOrUpdate(session);
 
