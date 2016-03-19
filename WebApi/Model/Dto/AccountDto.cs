a02701e8f7628aab120e31878ade01f2fac19002
Olivier Van Aken <oliver_va@live.be>
2016-03-19 18:15:37 +0100

    fixed InvitedSessions type

--- WebApi/Model/Dto/AccountDto.cs
+++ WebApi/Model/Dto/AccountDto.cs
@@ -10,7 +10,7 @@ namespace Kandoe.Web.Model.Dto {
         public string Surname { get; set; }
 
         public ICollection<ChatMessageDto> ChatMessages { get; set; }
-        public ICollection<AccountDto> InvitedSessions { get; set; }
+        public ICollection<SessionDto> InvitedSessions { get; set; }
         public ICollection<OrganisationDto> Organisations { get; set; }
         public ICollection<SessionDto> OrganisedSessions { get; set; }
         public ICollection<SessionDto> ParticipatingSessions { get; set; }
